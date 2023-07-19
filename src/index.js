90bf985b85387c8789e73d18e0133522f7a5fc69
Anzhalika Turlak <293852647@mail.ru>
2023-07-19 18:50:41 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    const numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const dozens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    const hundred = 'hundred';
+    let result = '';
+    let num = number;
+
+    function numberMinTwenty(number) {
+
+        if (number < 20 && number > 0) {
+            return numbers[number];
+        }
+        return '';
+    }
+
+    function numberMinHundred(number) {
+        if (number >= 20 && number < 100) {
+            if (number % 10 !== 0) {
+                return dozens[Math.floor(number / 10) - 2] + ' ' + numbers[number % 10];
+            } else {
+                return dozens[Math.floor(number / 10) - 2];
+            }
+        }
+        return '';
+    }
+
+    function numberMoreHundred(number) {
+        if (number >= 100) {
+            result = numbers[Math.floor(number / 100)] + ' ' + hundred;
+            number = number % 100;
+            return [result, number];
+        }
+        return [result, number];
+    }
+
+    if (num === 0) {
+        return numbers[0];
+    } else {
+        [result, num] = numberMoreHundred(num);
+        result += ' ' + numberMinHundred(num);
+        result += ' ' + numberMinTwenty(num);
+    }
+
+
+    return result.replace('  ', ' ').trim();
 }
