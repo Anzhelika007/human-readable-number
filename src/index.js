4d5939d0e7c4288233776c56ad299110a9fb1ddd
Anzhalika Turlak <293852647@mail.ru>
2023-07-21 10:47:27 +0300

    refactor: change method created tack add padStart and working with string

--- src/index.js
+++ src/index.js
@@ -1,46 +1,25 @@
 module.exports = function toReadable(number) {
     const numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-    const dozens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    const dozens = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
     const hundred = 'hundred';
+    let num = String(number).padStart(3, '0');
     let result = '';
-    let num = number;
-
-    function numberMinTwenty(number) {
-
-        if (number < 20 && number > 0) {
-            return numbers[number];
+    if (Number(num[0]) > 0) {
+        result += `${numbers[Number(num[0])]} ${hundred}`;
+    }
+    if (Number(num[1]) !== 0) {
+        if (Number(num[1]) === 1) {
+            return (result += ` ${numbers[Number(num.slice(1,))]}`).trim();
+        } else {
+            result += ` ${dozens[Number(num[1]) - 1]}`;
         }
-        return '';
+    }
+    if (Number(num[2]) > 0) {
+        result += ` ${numbers[Number(num[2])]}`
+    }
+    if (number === 0) {
+        return `${numbers[0]}`
     }
 
-    function numberMinHundred(number) {
-        if (number >= 20 && number < 100) {
-            if (number % 10 !== 0) {
-                return dozens[Math.floor(number / 10) - 2] + ' ' + numbers[number % 10];
-            } else {
-                return dozens[Math.floor(number / 10) - 2];
-            }
-        }
-        return '';
-    }
-
-    function numberMoreHundred(number) {
-        if (number >= 100) {
-            result = numbers[Math.floor(number / 100)] + ' ' + hundred;
-            number = number % 100;
-            return [result, number];
-        }
-        return [result, number];
-    }
-
-    if (num === 0) {
-        return numbers[0];
-    } else {
-        [result, num] = numberMoreHundred(num);
-        result += ' ' + numberMinHundred(num);
-        result += ' ' + numberMinTwenty(num);
-    }
-
-
-    return result.replace('  ', ' ').trim();
+    return result.trim()
 }
